5ae331c1a668f4c25716c0dd28daa58f952df575
Phantomical <phantom@lynches.ca>
2021-10-23 14:39:24 -0700

    Fully setup cross-compile mode

--- local-musl-toolchain.cmake
+++ local-musl-toolchain.cmake
@@ -1,10 +1,12 @@
 
 set(CMAKE_SYSROOT $ENV{WORKSPACE_DIR}/install)
 
-set(triplet x86_64-linux-unknown)
+set(triplet x86_64-unknown-linux-musl)
 
 set(CMAKE_C_COMPILER   $ENV{CC})
 set(CMAKE_CXX_COMPILER $ENV{CXX})
+set(CMAKE_C_COMPILER_TARGET   ${triplet})
+set(CMAKE_CXX_COMPILER_TARGET ${triplet})
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
